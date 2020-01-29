0ebcab3e4073c1cf98a69c0f33d0d5573c2b4b7e
yevheniinokhrin <233315@edu.p.lodz.pl>
2020-01-29 16:22:23 +0100

    created tests for customerTest

--- src/test/java/com/danix/code/smell/example001/CustomerTest.java
+++ src/test/java/com/danix/code/smell/example001/CustomerTest.java
@@ -1,6 +1,24 @@
 package com.danix.code.smell.example001;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * @author  danix
  */
-public class CustomerTest { }
+public class CustomerTest {
+    @Test
+    public void customerTest() {
+        Account account = getPremiumAccount(5);
+        Customer customer = CustomerTestUtils.getPersonCustomer(account);
+        assertThat(customer.getName(), is("danix"));
+        assertThat(customer.getEmail(), is("dan@mail.com"));
+    }
+
+    private Account getPremiumAccount(final int daysOverdrawn) {
+        AccountType normal = new AccountType(true);
+        return new Account(normal, daysOverdrawn);
+    }
+}

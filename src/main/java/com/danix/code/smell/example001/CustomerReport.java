dfe673dbfeaded00264316aae4760e6c884a527a
yevheniinokhrin <233315@edu.p.lodz.pl>
2020-01-29 16:04:28 +0100

    added sonarlint

--- src/main/java/com/danix/code/smell/example001/CustomerReport.java
+++ src/main/java/com/danix/code/smell/example001/CustomerReport.java
@@ -11,7 +11,7 @@ public class CustomerReport {
 
     private final Customer customer;
     private final Account account;
-    private final String line = "Account: IBAN: ";
+    private  static final String LINE = "Account: IBAN: ";
     public CustomerReport(@Nonnull final Customer customer, @Nonnull final Account account) {
         this.customer = checkNotNull(customer);
         this.account = checkNotNull(account);
@@ -20,7 +20,7 @@ public class CustomerReport {
     public String printCustomerDaysOverdrawn() {
         String fullName = customer.getFullName();
 
-        String accountDescription = line + account.getIban() + ", Days Overdrawn: "
+        String accountDescription = LINE + account.getIban() + ", Days Overdrawn: "
                 + account.getDaysOverdrawn();
         return fullName + accountDescription;
     }
@@ -28,7 +28,7 @@ public class CustomerReport {
     public String printCustomerMoney() {
         String fullName = customer.getFullName();
         String accountDescription = "";
-        accountDescription += line + account.getIban() + ", Money: " + account.getMoneyAmount();
+        accountDescription += LINE + account.getIban() + ", Money: " + account.getMoneyAmount();
         return fullName + accountDescription;
     }
 
